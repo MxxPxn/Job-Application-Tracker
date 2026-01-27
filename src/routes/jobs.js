e5ef16d9e6a33f79e7e4b281028223a99d8ebcf5
mxx <max3panri@outlook.com>
2026-01-26 21:07:36 -0500

    Add GET route for fetching job by ID

--- src/routes/jobs.js
+++ src/routes/jobs.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const jobsController = require('../controllers/jobController');
 
 router.post('/', jobsController.createJob);
+router.get('/', jobsController.getJobs);
+router.get('/:id', jobsController.getJobById);
 
 module.exports = router;
