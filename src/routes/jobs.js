48449bfd84befd58edfe6d037b27b6c1676d9171
mxx <max3panri@outlook.com>
2026-01-28 20:34:51 -0500

    added PUT route for update

--- src/routes/jobs.js
+++ src/routes/jobs.js
@@ -5,5 +5,6 @@ const jobsController = require('../controllers/jobController');
 router.post('/', jobsController.createJob);
 router.get('/', jobsController.getJobs);
 router.get('/:id', jobsController.getJobById);
+router.put('/:id', jobsController.updateJob);
 
 module.exports = router;
