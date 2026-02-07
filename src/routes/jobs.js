f53ea9ca2a841b09d7e0a798bb0dce5a863867ef
mxx <max3panri@outlook.com>
2026-02-06 22:04:13 -0500

    added asyncHandler

--- src/routes/jobs.js
+++ src/routes/jobs.js
@@ -1,11 +1,12 @@
+const { asyncHandler } = require ('../middleware/errorHandler');
+
 const express = require('express');
 const router = express.Router();
 const jobsController = require('../controllers/jobController');
 
-router.post('/', jobsController.createJob);
-router.get('/', jobsController.getJobs);
-router.get('/:id', jobsController.getJobById);
-router.put('/:id', jobsController.updateJob);
-router.delete('/:id', jobsController.deleteJob);
-
+router.post('/', asyncHandler(jobsController.createJob));
+router.get('/', asyncHandler(jobsController.getJobs));
+router.get('/:id', asyncHandler(jobsController.getJobById));
+router.put('/:id', asyncHandler(jobsController.updateJob));
+router.delete('/:id', asyncHandler(jobsController.deleteJob));
 module.exports = router;
