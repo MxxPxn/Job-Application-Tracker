9ddea4faff90e6faf7a8c53b8ae2209df5d8a61f
mxx <max3panri@outlook.com>
2026-02-16 08:38:51 -0500

    add verifyToken to routes

--- src/routes/jobs.js
+++ src/routes/jobs.js
@@ -1,12 +1,13 @@
-const { asyncHandler } = require ('../middleware/errorHandler');
+const { asyncHandler, verifyToken } = require ('../middleware/errorHandler');
 
 const express = require('express');
 const router = express.Router();
 const jobsController = require('../controllers/jobController');
-
+router.use(verifyToken);
 router.post('/', asyncHandler(jobsController.createJob));
 router.get('/', asyncHandler(jobsController.getJobs));
 router.get('/:id', asyncHandler(jobsController.getJobById));
 router.put('/:id', asyncHandler(jobsController.updateJob));
 router.delete('/:id', asyncHandler(jobsController.deleteJob));
+
 module.exports = router;
