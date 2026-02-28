9c050ba464f35093e6a4f4ca32870808ef2cbb36
mxx <max3panri@outlook.com>
2026-02-28 17:07:43 -0500

    fix test by loading dotenv in app.js and adding auth to test request

--- tests/jobs.test.js
+++ tests/jobs.test.js
@@ -2,9 +2,22 @@ const request = require('supertest');
 const app = require ('../src/app');
 
 describe('Job API Endpoints', () => {
+    let token;
+
+    beforeAll(async() => {
+        const res = await request(app)
+        .post('/api/auth/register')
+        .send({
+            email: `testuser${Date.now()}@example.com`,
+            password: 'TestPass123!'
+        });
+        token = res.body.token;
+    });
+
     it ('POST /api/jobs - should create a new job', async () => {
         const res = await request(app)
             .post('/api/jobs')
+            .set('Authorization', `Bearer ${token}`)
             .send({
                 company: 'Google',
                 position: 'developer',
@@ -20,17 +33,21 @@ describe('Job API Endpoints', () => {
     });
 
         it('GET /api/jobs - should retrieve all jobs', async () => {
-            const res = await request(app).get('/api/jobs');
+            const res = await request(app)
+                .get('/api/jobs')
+                .set('Authorization', `Bearer ${token}`);
 
             expect(res.statusCode).toEqual(200);
             expect(res.body).toHaveProperty('success', true);
             expect(Array.isArray(res.body.data)).toBe(true);
+            expect(res.body).toHaveProperty('pagination');
         });
 
         it('DELETE /api/jobs/:id - should delete a job', async () => {
             // First, create a job to delete
             const createRes = await request(app)
                 .post('/api/jobs')
+                .set('Authorization', `Bearer ${token}`)
                 .send({
                     company: 'Test Company',
                     position: 'Test Position',
@@ -42,7 +59,9 @@ describe('Job API Endpoints', () => {
 
             const jobId = createRes.body.data.id;
             
-            const deleteRes = await request(app).delete(`/api/jobs/${jobId}`);
+            const deleteRes = await request(app)
+                .delete(`/api/jobs/${jobId}`)
+                .set('Authorization', `Bearer ${token}`);
             expect(deleteRes.statusCode).toEqual(200);
             expect(deleteRes.body).toHaveProperty('success', true);
             expect(deleteRes.body.data).toHaveProperty('id', jobId);
