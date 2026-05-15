cb2a2bf12da2c253510cbfed5a4aeac4a55e332f
mxx <max3panri@outlook.com>
2026-05-15 15:08:27 -0400

    created new migraton file for source column

---
+++ migrations/1778713331961_add-source-to-jobs.js
@@ -0,0 +1,27 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+    pgm.addColumn('jobs', {
+        source: { type: 'varchar(100)',notNull: true,
+            default: 'unknown',}
+            
+
+    });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+    pgm.dropColumn('jobs', 'source');
+};
