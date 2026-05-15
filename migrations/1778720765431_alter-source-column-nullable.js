178ca6044e552089a8ee5b2461a550f38e6bf88e
mxx <max3panri@outlook.com>
2026-05-15 15:10:13 -0400

    add source column to jobs table and alter to nullable

---
+++ migrations/1778720765431_alter-source-column-nullable.js
@@ -0,0 +1,28 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+    pgm.alterColumn('jobs', 'source',{
+        notNull: false,
+        default: null,
+    });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+    pgm.alterColumn('jobs', 'source',{
+        notNull: true,
+        default: 'unknown',
+    });
+};
